08fd99a623f23c23dfc95836863f3cc287bf979a
tasarofimanesh <162376218+tasarofimanesh@users.noreply.github.com>
2024-04-07 10:45:15 +0330

    Update ssh-to-tun.sh
    rules to disable ipv6 were added

--- ssh-to-tun.sh
+++ ssh-to-tun.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #-----------------------------------------------
+# DIsable ipv6
+
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+#-----------------------------------------------
 #Check existance of ovpn and pass files
 
 if [ $(ls| grep nord.ovpn | wc -l) -ne 1 ]
